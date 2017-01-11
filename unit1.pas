e1c046240fc90d30eaef4c2f7f77a12893d0f619
Ilya T <tolstenkoiv@gmail.com>
2017-01-11 12:18:13 +0300

    initial project version

---
+++ unit1.pas
@@ -0,0 +1,84 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  Spin;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Edit1: TEdit;
+    FloatSpinEdit1: TFloatSpinEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Label1Click(Sender: TObject);
+    procedure Label2Click(Sender: TObject);
+    procedure Label4Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  label1.Caption:='Привет мир';
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  edit1.text:='';
+end;
+
+procedure TForm1.Label1Click(Sender: TObject);
+begin
+  halt(0);
+end;
+
+procedure TForm1.Label2Click(Sender: TObject);
+var
+  sep,gl,sl,s: string;
+  k,i: integer;
+begin
+  gl:='bdghklmnprstz';
+  sl:='aeiou';
+  sep:='-=+!@#$%^&*1234567890~_';
+  s:='';
+  Randomize;
+  for k:=1 to round(floatspinedit1.Value) do
+  begin
+    for i:=1 to 4 do
+        begin
+            s:=s+gl[random(13)+1]+sl[random(5)+1]
+        end;
+    if k<floatspinedit1.Value then s:=s+sep[random(length(sep))+1];
+  end;
+  edit1.Text:=s;
+
+end;
+
+procedure TForm1.Label4Click(Sender: TObject);
+begin
+
+end;
+
+end.
+
